dda52130be8c38164fdc2eceaa253b0079c76195
observantcacao <119044391+observantcacao@users.noreply.github.com>
2025-02-17 15:28:27 +0100

    ajout de mes modification

---
+++ Views/admin/Home.php
@@ -0,0 +1,8 @@
+<div class="container text-center mt-5 bg-dark">
+
+    <h1 class="mb-4">Page Administrative</h1>
+    <div class="d-grid gap-2">
+        <a href="/admin/logs" class="btn btn-primary">logs</a>
+        <a href="/admin/apiStatus" class="btn btn-primary">status Api</a>
+    </div>
+</div>
