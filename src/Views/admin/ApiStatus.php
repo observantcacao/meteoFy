2d8f6411eac012abea62bad136b871e8d26948b6
observantcacao <119044391+observantcacao@users.noreply.github.com>
2025-03-03 13:40:59 +0100

    Add files via upload

--- src/Views/admin/ApiStatus.php
+++ src/Views/admin/ApiStatus.php
@@ -7,4 +7,21 @@
     </nav>
     <h1 class="mb-4">Status des Api</h1>
 
+    <div class="container mt-5 d-flex justify-content-center gap-3">
+        <!-- Carte Verte -->
+        <div class="card text-white bg-success" style="max-width: 18rem;">
+            <div class="card-header">Spotify</div>
+            <div class="card-body">
+                <p class="card-text">Api n'a aucun soucis</p>
+            </div>
+        </div>
+
+        <!-- Carte Rouge -->
+        <div class="card text-white bg-danger" style="max-width: 18rem;">
+            <div class="card-header">Meteo</div>
+            <div class="card-body">
+                <p class="card-text">en cas d'erreur veuillez vérifier les informations depuis le site du fournisseur</p>
+            </div>
+        </div>
+    </div>
 </div>
--- src/Views/admin/Logs.php
+++ src/Views/admin/Logs.php
@@ -6,5 +6,42 @@
         </ol>
     </nav>
     <h1 class="mb-4">page de logs</h1>
-
+    <div class="container">
+    <?php if (!empty($logs)) :?>
+        <table class="table">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Niveau</th>
+                    <th scope="col">Message</th>
+                    <th scope="col">Canal</th>
+                    <th scope="col">Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($logs as $index => $log) : ?>
+                    <tr>
+                        <th scope="row"><?= $index + 1 ?></th>
+                        <td>
+                            <span class="badge bg-<?= Models\ActLogger::bootstrapClass($log['level_name']) ?>">
+                                <?= htmlspecialchars($log['level_name']) ?>
+                            </span>
+                        </td>
+                        <?php 
+                        $datetime = new DateTime($log['datetime']);
+                        $formattedTime = $datetime->format('H:i');
+                        ?>
+                        <td><?= htmlspecialchars($log['message']) ?></td>
+                        <td><?= htmlspecialchars($log['channel']) ?></td>
+                        <td><?= htmlspecialchars($formattedTime) ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else : ?>
+        <div class="alert alert-warning" role="alert">
+            Aucun log à afficher.
+        </div>
+    <?php endif; ?>
+    </div>
 </div>
