c24cdac1be4ab87dd0de9a996d097f0d80011a31
kelyan <kelyan.tchbl@eduge.ch>
2025-03-03 13:17:37 +0100

    ajout du logo dans le title

--- src/Views/Layout.php
+++ src/Views/Layout.php
@@ -5,8 +5,11 @@
   <meta charset="utf8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>MeteoFy</title>
+  <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/59/59860.png"type="image/x-icon" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+  
+
 </head>
 
 <body class="bg-dark text-white">
